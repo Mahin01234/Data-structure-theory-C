7119153323f0018976b5b5a0dcbf779b00e40894
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2025-10-05 13:17:27 +0600

    Update Signal_Link_List_Creating_a_Node.c

--- Introduction to Linked List in C/ Singly Linked List in C/Signal_Link_List_Creating_a_Node.c
+++ Introduction to Linked List in C/ Singly Linked List in C/Signal_Link_List_Creating_a_Node.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
 
 
 
@@ -14,40 +13,46 @@ struct Node
 
 
 
-
 int main()
 {
-    // static তিনটা নোড বানালাম
-    struct Node a, b, c;
+    // 3টা নোড ডাইনামিকালি তৈরি করব
+    struct Node* a = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* b = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* c = (struct Node*)malloc(sizeof(struct Node));
 
 
-
+    
     // data assign
-    a.value = 10;
-    b.value = 20;
-    c.value = 300;
+    a->value = 10;
+    b->value = 20;
+    c->value = 300;
 
 
-
-
+    
     // connection
-    a.next = &b;
-    b.next = &c;
-    c.next = NULL;
+    a->next = b;
+    b->next = c;
+    c->next = NULL;
+
+    // print values
+    printf("%d ", a->value);
+    printf("%d ", a->next->value);
+    printf("%d\n", a->next->next->value);
 
 
+    
+    // memory free করা ভালো অভ্যাস
+    free(a);
+    free(b);
+    free(c);
 
 
-    // print values using traversal
-    struct Node* temp = &a;  // head = a
-    while (temp != NULL)
-    {
-        printf("%d ", temp->value);
-        temp = temp->next;
-    }
-
-
-
-
+    
     return 0;
 }
+
+
+
+
+Output:
+10 20 300
