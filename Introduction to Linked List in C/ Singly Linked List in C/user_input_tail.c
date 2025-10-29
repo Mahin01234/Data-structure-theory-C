992a39e45f7d4f57ec8d5ab3991f280ac8166e92
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2025-10-29 16:30:12 +0600

    Update user_input_tail.c

--- Introduction to Linked List in C/ Singly Linked List in C/user_input_tail.c
+++ Introduction to Linked List in C/ Singly Linked List in C/user_input_tail.c
@@ -73,4 +73,23 @@ int main()
     printf("\n" ) ; 
 
     return 0 ; 
-}
+
+} 
+
+
+
+
+
+
+
+
+// Output : 
+// The element is : 2
+// The Node is 1 : 1
+// The Node is 2 : 2
+// The tail is : 1 2 
+// The new element is : 3
+// The Node is 1 : 3
+// The Node is 2 : 4
+// The Node is 3 : 5
+// After the new tail element is : 1 2 3 4 5
